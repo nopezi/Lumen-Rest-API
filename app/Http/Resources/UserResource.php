41487faafa542f5d61c76301b136bb4507e4a2ba
nopezi <sopezi@gmail.com>
2020-07-08 00:19:34 +0700

    commit pertama

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * 
+ */
+class UserResource extends JsonResource
+{
+	
+	public function toArray($request) {
+		return [
+			'id'       => $this->id,
+			'username' => $this->username,
+			'email'	   => $this->email, 
+		];
+	}
+}
